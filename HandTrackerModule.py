17f5a83161762a97fa7347b828ea34923a91d930
vikasjoshis001 <vikasjoshis001@gmail.com>
2022-02-05 11:37:12 +0530

    Minor change in module

--- HandTrackerModule.py
+++ HandTrackerModule.py
@@ -45,19 +45,7 @@ def main():
         success, img = cap.read()
         img = detector.findHands(img)
         lmList = detector.findPosition(img)
-        if len(lmList) != 0\
-
-
-
-
-
-
-
-
-
-
-
-                :
+        if len(lmList) != 0:
             print(lmList[4])
         cTime = time.time()
         fps = 1 / (cTime - pTime)
